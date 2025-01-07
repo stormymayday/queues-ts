eebe3ba5e08ad22218a60c3b6548a99ced5c50a4
stormymayday <stormymayday7@gmail.com>
2025-01-06 20:28:09 -0800

    refactored to work with different types

--- implementations/scott-barret/link-list-based.ts
+++ implementations/scott-barret/link-list-based.ts
@@ -1,24 +1,24 @@
-class Node {
-    value: number;
-    next: Node | null;
-    constructor(value: number) {
+class Node<T> {
+    value: T;
+    next: Node<T> | null;
+    constructor(value: T) {
         this.value = value;
         this.next = null;
     }
 }
 
-export default class Queue {
-    first: Node | null;
-    last: Node | null;
+export default class Queue<T> {
+    first: Node<T> | null;
+    last: Node<T> | null;
     length: number;
-    constructor(value: number) {
+    constructor(value: T) {
         const newNode = new Node(value);
         this.first = newNode;
         this.last = newNode;
         this.length = 1;
     }
 
-    enqueue(value: number): Queue {
+    enqueue(value: T): Queue<T> {
         const newNode = new Node(value);
         if (!this.last) {
             this.first = newNode;
@@ -31,7 +31,7 @@ export default class Queue {
         return this;
     }
 
-    dequeue(): Node | null {
+    dequeue(): Node<T> | null {
         if (!this.first) {
             return null;
         }
